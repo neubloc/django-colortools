500a3403fd80f0d98e403d28d188e758ffef1155
Szymon Rajchman <srajchman@neubloc.com>
2011-06-14 15:20:20 +0200

    added support for global fixtures

--- colortools/tests/__init__.py
+++ colortools/tests/__init__.py
@@ -1,1 +1,3 @@
 from colortools.tests.test import ColorTextTestResultTestCase
+from colortools.tests.test import ColorDjangoTestSuiteRunnerTestCase
+from colortools.tests.test import FixtureListFunctionTestCase
